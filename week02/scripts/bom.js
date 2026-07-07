9439bace5dcecadad0de34f3ad1fd747f9098186
knatagm <knatagm@gmail.com>
2026-07-07 10:47:14 -0500

    bom

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,14 +2,25 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
+button.addEventListener('click', function() {
 
-const deleteButton = document.createElement('button');
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
+    li.textContent = input.value;
 
-deleteButton.textContent = '❌';
+    deleteButton.textContent = '❌';
 
-li.append(deleteButton);
+    li.append(deleteButton);
 
-list.append(li);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function() {
+        list.removeChild(li);
+        input.focus();
+    });
+
+    input.value = '';
+    input.focus();
+    
+});
